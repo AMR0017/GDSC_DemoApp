c93dfa66f825d13a4246405a4c06c1a143ff3ca3
AMR0017 <azhar.mr12@gmail.com>
2024-02-10 18:21:15 +0800

    Add Toast

--- app/src/main/java/com/example/gdsc_demoapp/MainActivity.kt
+++ app/src/main/java/com/example/gdsc_demoapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener {
             val intent = Intent(this@MainActivity, HomepageActivity::class.java)
+            Toast.makeText(this, "Login Success!!", Toast.LENGTH_SHORT)
             startActivity(intent)
+
         }
     }
 }
